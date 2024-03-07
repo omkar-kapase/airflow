6ffa83c000d808a66b73e35cb4b914b6e7ce4f43
omkar-kapase <118868018+omkar-kapase@users.noreply.github.com>
2024-03-07 15:03:36 +0530

    Update mysql_script.py

--- airflow/dags/mysql_script.py
+++ airflow/dags/mysql_script.py
@@ -1,30 +1,3 @@
-from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
-from datetime import datetime
-from mysql_script import execute_mysql_query
-
-# Define your MySQL connection string ID
-mysql_conn_id = 'mysql_conn'
-
-
-# Define your DAG
-dag = DAG(
-    'mysql_insert_dag',
-    description='A DAG to insert values into MySQL table',
-    schedule_interval=None,
-    start_date=datetime(2024, 2, 28),
-    catchup=False
-)
-
-# Define a task using PythonOperator and link it to your Python function
-mysql_insert_task = PythonOperator(
-    task_id='mysql_insert_task',
-    python_callable=execute_mysql_query,
-    dag=dag,
-    retries=3
-)
-
-#root@ubuntugui:~/airflow/dags# cat mysql_script.py
 import mysql.connector
 
 def execute_mysql_query():
@@ -39,7 +12,8 @@ def execute_mysql_query():
 
     # Insert values into the table
     query = "INSERT INTO testing (job_id, job_name) VALUES (%s, %s)"
-    values =(114, "dog")
+    values =(115, "cat"),
+            
     cursor.execute(query, values)
 
     print('Values uploaded')
